35f80ba45e6387c3e838062c1fc0cacb7ebf8d1e
phumebyte <phumelele.dlamini1000@gmail.com>
2024-11-17 16:16:41 +0200

    Added more routing to return to all Vans

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -1,60 +1,42 @@
 import React from "react"
-import { Link, useSearchParams } from "react-router-dom"
+import { Link, useParams, useLocation } from "react-router-dom"
 
-export default function Vans() {
-    const [searchParams, setSearchParams] = useSearchParams()
-    const [vans, setVans] = React.useState([])
+export default function VanDetail() {
+    const params = useParams()
+    const location = useLocation()
+    console.log(location)
     
-    const typeFilter = searchParams.get("type")
-    
+    const [van, setVan] = React.useState(null)
+
     React.useEffect(() => {
-        fetch("/api/vans")
+        fetch(`/api/vans/${params.id}`)
             .then(res => res.json())
-            .then(data => setVans(data.vans))
-    }, [])
-    
-    const displayedVans = typeFilter
-        ? vans.filter(van => van.type === typeFilter)
-        : vans
+            .then(data => setVan(data.vans))
+    }, [params.id])
 
-    const vanElements = displayedVans.map(van => (
-        <div key={van.id} className="van-tile">
-            <Link to={`/vans/${van.id}`}>
-                <img src={van.imageUrl} />
-                <div className="van-info">
-                    <h3>{van.name}</h3>
-                    <p>${van.price}<span>/day</span></p>
-                </div>
-                <i className={`van-type ${van.type} selected`}>{van.type}</i>
-            </Link>
-        </div>
-    ))
-   
+    const search = location.state?.search || ""
+    const type = location.state?.type || "all"
+    
     return (
-        <div className="van-list-container">
-            <h1>Explore our van options</h1>
-            <div className="van-list-filter-buttons">
-                <button 
-                    onClick={() => setSearchParams({type: "simple"})}
-                    className="van-type simple"
-                >Simple</button>
-                <button 
-                    onClick={() => setSearchParams({type: "luxury"})}
-                    className="van-type luxury"
-                >Luxury</button>
-                <button 
-                    onClick={() => setSearchParams({type: "rugged"})}
-                    className="van-type rugged"
-                >Rugged</button>
-                <button 
-                    onClick={() => setSearchParams({})}
-                    className="van-type clear-filters"
-                >Clear filter</button>
+        <div className="van-detail-container">
+            <Link
+                to={`..${search}`}
+                relative="path"
+                className="back-button"
+            >&larr; <span>Back to {type} vans</span></Link>
             
-            </div>
-            <div className="van-list">
-                {vanElements}
-            </div>
+            {van ? (
+                <div className="van-detail">
+                    <img src={van.imageUrl} />
+                    <i className={`van-type ${van.type} selected`}>
+                        {van.type}
+                    </i>
+                    <h2>{van.name}</h2>
+                    <p className="van-price"><span>${van.price}</span>/day</p>
+                    <p>{van.description}</p>
+                    <button className="link-button">Rent this van</button>
+                </div>
+            ) : <h2>Loading...</h2>}
         </div>
     )
 }
